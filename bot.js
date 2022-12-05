2c10a9dd0f098590df5e131d60d099ec213d23d3
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2022-12-05 12:22:47 +0100

    Working double media tweet

--- bot.js
+++ bot.js
@@ -1,13 +1,19 @@
 import { TwitterApi } from 'twitter-api-v2';
 
-const userClient = new TwitterApi({
+const client = new TwitterApi({
     appKey: '6ReZstRFPkbTrdw5jrBwYp65T',
     appSecret: 'MSeUDwTbNz34LTeUpdKJ48csoF9e3yTm2R36xt4CIQ1gbp8Sql',
     accessToken: '1599691376639315968-GMo53j4SmadueNLb6Nv4CQHDSCLSAK',
     accessSecret: 'dhPdlueO6VUQtB1CbsX6vDvesCtagWzOe3Rq3HZOdomIL',
 });
 
-const { data: createdTweet } = await userClient.v2.tweet('Sondage', {
-    poll: { duration_minutes: 120, options: ['Oui', 'Non'] },
-});
-console.log('Tweet', createdTweet.id, ':', createdTweet.text);
+async function postTweet(text) {
+    const media1 = await client.v1.uploadMedia('./media/persons_solo/1.png');
+    const media2 = await client.v1.uploadMedia('./media/persons_solo/4.png');
+
+    await client.v2.tweetThread([
+        { text: text, media: { media_ids: [media1, media2] } },
+      ]);
+}
+
+postTweet('Comment ça mon Grim ?');
