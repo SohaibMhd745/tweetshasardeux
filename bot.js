6b7a9e5d0c0fadbf2e888ce83ed73d505e446346
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2022-12-05 17:59:55 +0100

    Clean working random tweet

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 import { TwitterApi } from 'twitter-api-v2';
+import { persons_s } from './lists/persons/persons_s.js';
+import { actions_s } from './lists/actions/actions_s.js';
 
 const client = new TwitterApi({
     appKey: '6ReZstRFPkbTrdw5jrBwYp65T',
@@ -7,13 +9,31 @@ const client = new TwitterApi({
     accessSecret: 'dhPdlueO6VUQtB1CbsX6vDvesCtagWzOe3Rq3HZOdomIL',
 });
 
-async function postTweet(text) {
-    const media1 = await client.v1.uploadMedia('./media/persons_solo/1.png');
-    const media2 = await client.v1.uploadMedia('./media/persons_solo/4.png');
-
-    await client.v2.tweetThread([
-        { text: text, media: { media_ids: [media1, media2] } },
-      ]);
+async function prepareTweet() {
+    const person = persons_s[Math.floor(Math.random() * persons_s.length)];
+    const action = actions_s[Math.floor(Math.random() * actions_s.length)];
+    postTweet(
+        action.str1 + ' ' + person.str + ' ' + action.str2,
+        ['persons/1', 'persons/4']
+    );
 }
 
-postTweet('Comment ça mon Grim ?');
+async function postTweet(text, mediasArray) {
+    if (mediasArray) {
+        await client.v2.tweetThread([
+            {
+                text: text,
+                media: {
+                    media_ids: [
+                        await client.v1.uploadMedia('./media/' + mediasArray[0] + '.png'),
+                        await client.v1.uploadMedia('./media/' + mediasArray[1] + '.png')
+                    ]
+                }
+            }
+        ]);
+    } else {
+        await client.v1.tweet(text);
+    }
+}
+
+setInterval(prepareTweet, 1000 * 60 * 30);
